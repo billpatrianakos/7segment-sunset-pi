456139dfa63180c0042560f333617c5e45661f6d
Bill Patrianakos <bill@billpatrianakos.me>
2019-09-22 13:17:41 -0500

    Get basic display logic coded before getting into the fun stuff.

---
+++ display.py
@@ -0,0 +1,22 @@
+##
+# Main Module: display.py
+##
+
+import configparser
+import tm1637
+from sys import sleep
+
+config = configparser.ConfigParser()
+config.read('config/appconfig.ini')
+
+CLK = config['GPIO']['clk']
+DIO = config['GPIO']['dio']
+
+tm = tm1637.TM1637(clk=CLK, dio=DIO)
+
+tm.brightness(7)
+
+while True:
+    tm.scroll('HELLO LISA')
+    tm.numbers(69, 69)
+    sleep(3)
